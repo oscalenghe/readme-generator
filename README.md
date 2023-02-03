95a7be7d46653176ba8805b0f36ba9fdf8151fe7
oscalenghe <42279508+oscalenghe@users.noreply.github.com>
2023-02-02 21:21:23 -0500

    cleaned questions, added fs.write file

---
+++ README.md
@@ -0,0 +1,41 @@
+# test
+
+
+
+  ## Table of Contents
+  
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contribution)
+  - [Questions](#questions)
+
+  ## Description
+  
+  test
+  
+  ## Installation
+  
+  test
+
+  ## Usage
+
+  test
+  
+  ## License
+
+ MIT
+  
+  ## Contribution
+  
+  we
+  
+  ## Questions
+  
+  If you have any questions, you can contact me via the information below.
+
+  // TODO: Add link to github with username 
+  * Email: we
+  * Github: https://github.com/we
+}}
--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const fs = require('fs');
 
 
 
-let readmeWrapper = (data) =>
- `function generateMarkdown(data) {
-    return '# ${data.title}
+let readmeWrapper = (data) => 
+`# ${data.title}
 
-  ${licenseGenerator.checkLicense(data.license)}
+
 
   ## Table of Contents
   
@@ -33,7 +32,7 @@ let readmeWrapper = (data) =>
   
   ## License
 
- ${licenseGenerator.licenseContent(data.license)}
+ ${data.license}
   
   ## Contribution
   
@@ -44,63 +43,59 @@ let readmeWrapper = (data) =>
   If you have any questions, you can contact me via the information below.
 
   // TODO: Add link to github with username 
-  * Email: ${data.email}';
+  * Email: ${data.email}
   * Github: https://github.com/${data.github}
-}`;
+}}`;
+    
 
-
-
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Project title?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Project description?',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Installation guide?',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Project usage?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Email for questions?',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Github username for questions?',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'License?',
-        choices:['MIT', 'ISC', 'GNUPLv3'],
-        filter(val){
-            return val.toLowerCase();
-                }
-    }
-]
-
-function runQuery() {
-    return inquirer.prompt(questions)
-    .then((answers)=>{
-        console.log(answers)
-        return answers
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
+inquirer.prompt([
+{
+    type: 'input',
+    name: 'title',
+    message: 'Project title?',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Project description?',
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Installation guide?',
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Project usage?',
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'License?',
+    choices:['MIT', 'ISC', 'GNUPLv3'],
+},
+{
+    type: 'input',
+    name: 'contribute',
+    message: 'Contributions?',
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Email for questions?',
+},
+{
+    type: 'input',
+    name: 'github',
+    message: 'Github username for questions?',
 }
+])
+.then((answers) => {
+   const userInput = readmeWrapper(answers)
 
-runQuery();
+
+fs.writeFile('README.md', userInput, (err) =>
+  err ? console.error(err) : console.log('Success!')
+); 
+})
